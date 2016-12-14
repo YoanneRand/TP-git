6d7c390ca0a5e194a0b595a1bbb9fd87837fb275
Yoanne Randria <yoanne.randria@gmail.com>
2016-12-14 15:52:56 +0100

    add partner name

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,4 @@
- ### application.bat - 2016 - Yoanne Randria ###
+### application.bat - 2016 - Yoanne Randria ###
 echo "Copyright imie - 2016"
 echo "Build: 10:00"
-echo "version 1.1"
+echo "version 1.2"
